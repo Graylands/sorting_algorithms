4edbea0c559d05c2d3730f5ee4b8cf57fd54d038
Graylands <graylandsgreen@gmail.com>
2022-07-19 17:26:06 -0700

    Last commit

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,70 +1,51 @@
 #include "sort.h"
+
 /**
- * check_tree - swiftdown check
- * @array: pointer to array
- * @size: size of the pointer
- * @size_init: original size of the array
- * @i: index as a root of the tree
- *
-**/
-void check_tree(int *array, size_t size_init, size_t size, size_t i)
+* stupify - recurrssive heapfiy function
+* @array: Array to sort
+* @heap: size of heap data
+* @i: index
+* @size: size of array
+*/
+
+void stupify(int *array, int heap, int i, int size)
 {
+	int lar = i, left = 2 * i + 1, right = 2 * i + 2, t;
 
-	int n, branch1, branch2;
-	size_t br1, br2;
-
-	br1 = i * 2 + 1;
-	br2 = br1 + 1;
-	branch1 = array[br1];
-	branch2 = array[br2];
-	if (((br1 < size) && (br2 < size) &&
-		(branch1 >= branch2 && branch1 > array[i]))
-		|| ((br1 == size - 1) && branch1 > array[i]))
+	if (left < heap && array[left] > array[lar])
+		lar = left;
+	if (right < heap && array[right] > array[lar])
+		lar = right;
+	if (lar != i)
 	{
-		n = array[i];
-		array[i] = branch1;
-		array[br1] = n;
-		print_array(array, size_init);
+		t = array[i], array[i] = array[lar], array[lar] = t;
+		print_array(array, size);
+		stupify(array, heap, lar, size);
 	}
-	else if ((br1 < size) && (br2 < size) &&
-		(branch2 > branch1 && branch2 > array[i]))
-	{
-		n = array[i];
-		array[i] = branch2;
-		array[br2] = n;
-		print_array(array, size_init);
-	}
-	if (br1 < size - 1)
-		check_tree(array, size_init, size, br1);
-	if (br2 < size - 1)
-		check_tree(array, size_init, size, br2);
 }
+
 /**
- * heap_sort - sorts an array of integers
- * in ascending order using the Heap
- * sort algorithm
- * @array: pointer to array
- * @size: size of the pointer
- *
-**/
+* heap_sort - Sorts array with heap sort algo
+* @array: array to sort
+* @size: Size of array to sort
+*/
+
 void heap_sort(int *array, size_t size)
 {
-	size_t i, size_init = size;
-	int n;
+	int i = size / 2 - 1, temp;
 
-	if (!array)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size / 2 ; i++)
+	for (; i >= 0; i--)
+		stupify(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
 	{
-		check_tree(array, size_init, size, size / 2 - 1 - i);
-	}
-	for (i = 0; i < size_init - 1; i++)
-	{
-		n = array[0];
-		array[0] = array[size - 1 - i];
-		array[size - 1 - i] = n;
-		print_array(array, size_init);
-		check_tree(array, size_init, size - i - 1, 0);
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		if (i > 0)
+			print_array(array, size);
+		stupify(array, i, 0, size);
 	}
 
 }
